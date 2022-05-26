03aa5b373da1099426bb2e03c5b2e4b61ad8bfe1
Andres Lugo <roniber138@hotmail.com>
2022-05-26 01:07:15 -0500

    AdjustVerticalAlignIconsInputs

--- pages/register.tsx
+++ pages/register.tsx
@@ -6,19 +6,19 @@ const Register = () => (
         <div className="bg-icon w-[150px] h-[150px] mx-[auto] mt-[10vh]"></div>
         <div className="name-company text-center"></div>
         <form className="flex flex-col mt-10">
-            <div className="input-blue flex flex-row bg-blue-seos  w-[20rem] mx-[auto] mt-6 br-[5px]">
+            <div className="input-blue flex flex-row items-center bg-blue-seos  w-[20rem] mx-[auto] mt-6 br-[5px]">
                 <FontAwesomeIcon className="w-[20px]" icon={faUser}/>
                 <input type="text" className="bg-blue-seos" placeholder="Usuario"></input>
             </div>
-            <div className="input-blue flex flex-row bg-blue-seos  w-[20rem] mx-[auto] mt-6 br-[5px]">
+            <div className="input-blue flex flex-row items-center bg-blue-seos  w-[20rem] mx-[auto] mt-6 br-[5px]">
                 <FontAwesomeIcon className="w-[20px]" icon={faKey}/>
                 <input type="text" className="bg-blue-seos" placeholder="Clave"></input>
             </div>
-            <div className="input-blue flex flex-row bg-blue-seos  w-[20rem] mx-[auto] mt-6 br-[5px]">
+            <div className="input-blue flex flex-row items-center bg-blue-seos  w-[20rem] mx-[auto] mt-6 br-[5px]">
                 <FontAwesomeIcon className="w-[20px]" icon={faEnvelope}/>
                 <input type="email" className="bg-blue-seos" placeholder="Correo"></input>
             </div>
-            <div className="input-blue flex flex-row bg-blue-seos  w-[20rem] mx-[auto] mt-6 br-[5px]">
+            <div className="input-blue flex flex-row items-center bg-blue-seos  w-[20rem] mx-[auto] mt-6 br-[5px]">
                 <FontAwesomeIcon className="w-[20px]" icon={faMobileScreenButton}/>
                 <input type="tel" className="bg-blue-seos" placeholder="Celular"></input>
             </div>
