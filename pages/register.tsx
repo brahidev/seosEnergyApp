b37b5bbb00f45bf47ba079dd6e39364354c2cd1c
Andres Lugo <roniber138@hotmail.com>
2022-06-02 22:17:04 -0500

    FooterComponent

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,11 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-<<<<<<< HEAD
 import { faUser, faKey, faEnvelope, faMobileScreenButton, faCircleQuestion } from '@fortawesome/free-solid-svg-icons'
-=======
-import { faUser, faKey, faEnvelope, faMobileScreenButton } from '@fortawesome/free-solid-svg-icons'
-
 import Footer from '../components/Footer'
->>>>>>> fd5d2add6b1c75a95f989bd83d29d7647d4f9914
 
 const Register = () => (
     <>
@@ -30,16 +25,7 @@ const Register = () => (
             </div>
             <button className="btn-submit w-[15rem] mx-[auto] mt-10">CREAR CUENTA</button>
         </form>
-        <section className="flex flex-row justify-between w-[100vw] fixed top-[95vh]">
-            <a className="flex flex-row mx-[1rem]" href="#">
-                <FontAwesomeIcon className="w-[20px] mx-[0.5rem] text-blue-seos" icon={faUser as IconProp} />  
-                <span className="text-blue-dark-seos text-[1.2rem] font-bold">Entrar</span>
-            </a>
-            <a className="flex flex-row mx-[1rem]" href="#">
-                <FontAwesomeIcon className="w-[20px] mx-[0.5rem] text-blue-seos" icon={faCircleQuestion as IconProp}/> 
-                <span className="text-blue-dark-seos text-[1.2rem] font-bold">Ayuda</span>
-            </a>
-        </section>
+        <Footer />
     </>
 )
 
